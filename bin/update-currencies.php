3d44744c3b5abc29db3df41005108a57ad5e119a
Alan Bondarchuk <imacoda@gmail.com>
2017-09-09 22:17:51 +0500

    Minor cs fixes

--- bin/update-currencies.php
+++ bin/update-currencies.php
@@ -1,18 +1,18 @@
 <?php
 
-use IsoCurrency\Generation\Client;
+use IsoCurrency\Generation\Client as CurrencyIsoClient;
+use IsoCurrency\Generation\Country;
 use IsoCurrency\Generation\Generator;
-use IsoCurrency\IsoCurrency;
 
 require_once __DIR__."/../vendor/autoload.php";
 
-$client = new Client();
-/** @var \IsoCurrency\Generation\Country[] $countries */
-$countries = $client->fetch();
+/** @var Country[] $countries */
+$countries = (new CurrencyIsoClient())->fetch();
 $currencies = [];
 foreach ($countries as $country) {
-    $currencies[] = [
-        'alphabeticCode' => $country->getAlphabeticCode(),
+    $code = $country->getAlphabeticCode();
+    $currencies[$code] = [
+        'alphabeticCode' => $code,
         'minorUnit' => $country->getMinorUnit(),
         'numericCode' => $country->getNumericCode(),
     ];
@@ -22,7 +22,7 @@ $generator = new Generator();
 $generator->generate(
     'IsoCurrency.php.twig',
     [
-        'currencyCodes' => array_keys($countries),
+        'currencyCodes' => array_keys($currencies),
         'currencies' => $currencies,
     ]
 );
