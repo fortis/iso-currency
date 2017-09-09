8be92d72b4011f0f6ef9c50985e3537433b83b1e
Alan Bondarchuk <imacoda@gmail.com>
2017-09-10 01:37:01 +0500

    Add InvalidCurrencyException

---
+++ src/Exception/InvalidCurrencyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IsoCurrency\Exception;
+
+/**
+ * Class InvalidCurrencyException
+ * @package IsoCurrency\Exception
+ */
+class InvalidCurrencyException extends \Exception
+{
+
+}
