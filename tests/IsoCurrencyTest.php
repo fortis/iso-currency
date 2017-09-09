fb1a48b499ef2154ae8bddd100ea719bb610fc48
Alan Bondarchuk <imacoda@gmail.com>
2017-09-10 01:39:23 +0500

    Add unit tests

---
+++ tests/IsoCurrencyTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace IsoCurrency\Tests;
+
+use IsoCurrency\Exception\InvalidCurrencyException;
+use IsoCurrency\IsoCurrency;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for class Money.
+ */
+class IsoCurrencyTest extends TestCase
+{
+    public function testCreateInstance()
+    {
+        $this->assertInstanceOf(
+            IsoCurrency::class,
+            new IsoCurrency('USD')
+        );
+
+        $this->assertInstanceOf(
+            IsoCurrency::class,
+            IsoCurrency::create('USD')
+        );
+
+        $this->assertInstanceOf(
+            IsoCurrency::class,
+            IsoCurrency::USD()
+        );
+    }
+
+    public function testUndefinedCurrency()
+    {
+        $this->expectException(InvalidCurrencyException::class);
+        IsoCurrency::create('UASD');
+    }
+
+    public function testEmptyCurrency()
+    {
+        $this->expectException(InvalidCurrencyException::class);
+        IsoCurrency::create('');
+    }
+
+    public function testCustomMinorUnit()
+    {
+        $usd = IsoCurrency::USD(0);
+        $this->assertEquals(0, $usd->getMinorUnit());
+    }
+
+    public function testNegativeMinorUnit()
+    {
+        $this->expectException(InvalidCurrencyException::class);
+        IsoCurrency::USD(-2);
+    }
+
+    public function testIs()
+    {
+        $currency = IsoCurrency::USD();
+        $this->assertTrue($currency->is(IsoCurrency::create('USD')));
+    }
+}
