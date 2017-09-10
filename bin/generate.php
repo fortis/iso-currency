c93c35d8e0a1be449b272e2b897d7b4929fe94bd
Alan Bondarchuk <imacoda@gmail.com>
2017-09-10 22:22:33 +0500

    Add CurrencyCode:: generation

--- bin/generate.php
+++ bin/generate.php
@@ -3,9 +3,9 @@
 use GuzzleHttp\Client as GuzzleClient;
 use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
-use IsoCurrency\Generation\CurrencyIsoApiClient;
-use IsoCurrency\Generation\FileWriter;
-use IsoCurrency\Generation\Generator;
+use Currency\Generation\CurrencyIsoApiClient;
+use Currency\Generation\FileWriter;
+use Currency\Generation\Generator;
 
 require_once __DIR__."/../vendor/autoload.php";
 
@@ -15,16 +15,15 @@ try {
     $currencyIsoClient = new CurrencyIsoApiClient($adapter, new GuzzleMessageFactory);
     $loader = new Twig_Loader_Filesystem(__DIR__.'/../src/Resources/templates');
     $twig = new Twig_Environment($loader);
-    $fileObject = new \SplFileObject(__DIR__.'/../src/IsoCurrency.php', 'w');
 
-    $generator = new Generator(
-        $currencyIsoClient,
-        $twig,
-        $fileObject,
-        'IsoCurrency.php.twig'
-    );
+    $generator = new Generator($currencyIsoClient, $twig);
 
-    $generator->generate();
+    $basePath = __DIR__.'/../src';
+    $fileObject = new \SplFileObject($basePath.'/Currency.php', 'w');
+    $generator->generate('Currency.php.twig', $fileObject);
+
+    $fileObject = new \SplFileObject($basePath.'/CurrencyCode.php', 'w');
+    $generator->generate('CurrencyCode.php.twig', $fileObject);
 } catch (\Exception $exception) {
     error_log($exception->getMessage());
 }
